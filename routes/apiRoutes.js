bda34a2ebc1bc41486cb74f46f9858b7e10a0632
Sam <Sambose426@yahoo.com>
2023-09-24 00:52:21 -0600

    added a delete route for note/

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,9 +3,8 @@ const path = require("path");
 const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
 
-const dbFile = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json")));
-
 router.get("/notes", (req, res) => {
+	const dbFile = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json")));
 	try {
 		res.status(200).json(dbFile);
 	} catch (err) {
@@ -14,6 +13,7 @@ router.get("/notes", (req, res) => {
 });
 
 router.post("/notes", async (req, res) => {
+	const dbFile = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json")));
 	try {
 		const id = uuidv4();
 		const body = { ...req.body, id };
@@ -22,6 +22,20 @@ router.post("/notes", async (req, res) => {
 			if (err) throw new ERROR();
 		});
 		res.status(200).json(dbFile);
+	} catch (err) {
+		if (err) res.status(400).json(err);
+	}
+});
+
+router.delete("/notes/:id", (req, res) => {
+	const dbFile = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json")));
+	try {
+		const updatedDB = dbFile.filter((note) => note.id !== req.params.id);
+		if (updatedDB.length === dbFile.length) return res.status(200).send("No note with that id was found");
+		fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(updatedDB, null, 2), (err) => {
+			if (err) throw new ERROR();
+		});
+		res.status(200).json(updatedDB);
 	} catch (err) {
 		if (err) res.status(500).json(err);
 	}
